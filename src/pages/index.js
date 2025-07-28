9e7140224a22b945fdb8f02524e2f29ff1c93576
katefrommoon <katefrommoon@users.noreply.github.com>
2025-07-28 20:35:34 +0000

    deploy: 7f2e6a2a9d1cfbf64390d26f9fa18232520763dc

--- src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export * from './main.page';
-export * from './modal.page';
-export * from './product.page';
--- src/services/index.js
+++
@@ -1,6 +0,0 @@
-export * from './challenger.service';
-export * from './get.service';
-export * from './post.service';
-export * from './put.service';
-export * from './delete.service';
-export * from './patch.service';
